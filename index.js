498083a5730074d002cddd9d14d569278e1ca1ec
VsIG <Valentyn2202@gmail.com>
2020-05-14 19:01:30 +0300

    Little work

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf')
 
-//console.log(process.env);
-//token=process.env.TOKEN;
+bot.use(Telegraf.log())
 
 const bot = new Telegraf(process.env.TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('Hi, my beloved human. I`m here to help make Your own To-Do Lists! To make new List write:"/newList"'))
+bot.command('newList', (ctx) => ctx.reply('Bot started.'));
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
