11ed5ccb762c1ea087f89266228a5bff462b52e9
VsIG <Valentyn2202@gmail.com>
2020-05-14 13:14:00 +0300

    Switched from config to .env

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+'use strict'
+
 require('dotenv').config();
-console.log(process.env);
+const { Telegraf } = require('telegraf')
+
+//console.log(process.env);
+//token=process.env.TOKEN;
+
+const bot = new Telegraf(process.env.TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
